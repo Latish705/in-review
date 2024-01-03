9a1000b5889397946b22f19f27f93660556be6d6
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-03 21:46:02 +0530

    small fix

--- client/app/askQuestion/page.jsx
+++ client/app/askQuestion/page.jsx
@@ -51,9 +51,12 @@ const askQuestion = () => {
             id="name"
           ></input>
         </div>
-        <button className="text-white px-8 py-2 rounded-xl bg-black m-1">
-          Ask
-        </button>
+        <button
+        type="button"
+        className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >
+        Ask
+      </button>
       </form>
     </div>
   );
