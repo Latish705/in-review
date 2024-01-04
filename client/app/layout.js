b222fa858e012e58dc9d4c3dbd7c609f662e474d
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-04 10:10:14 +0530

    font weights updated for krub

--- client/app/layout.js
+++ client/app/layout.js
@@ -2,7 +2,7 @@ import { Krub, Inter } from 'next/font/google';
 import { Karma } from 'next/font/google'
 import './globals.css'
 
-const krub = Krub({ subsets: ['latin'], weight: '700' })
+const krub = Krub({ subsets: ['latin'], weight: ['700', '500'] })
 export const inter = Inter({ subsets: ['latin'], weight: '400' });
 export const karma = Karma({ subsets: ['latin'], weight: "300"})
 
