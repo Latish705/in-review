946ed36a4672964c807c6f57733c4f7a8cb650d0
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-04 06:11:21 +0530

    navbar styling improved

--- client/app/ui/navbar.js
+++ client/app/ui/navbar.js
@@ -2,8 +2,15 @@
 
 export default function Navbar() {
     return(
-        <div>
+        <div className="flex justify-between items-center px-10 py-8 text-white ">
             <span>In-Review</span>
+
+            <div className="flex gap-5">
+                <span>Home</span>
+                <span>About</span>
+                <span>Login</span>
+                <span>Signup</span>
+            </div>
         </div>
     )
 }
