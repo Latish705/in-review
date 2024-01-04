86b5fb912a3075927e1fafed897bfec32accc92a
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-04 16:52:25 +0530

    Fix questionController exports

--- server/src/routes/question.routes.js
+++ server/src/routes/question.routes.js
@@ -1,7 +1,25 @@
 import { Router } from "express";
-import { userQuestion } from "../controllers/questionController.js";
+import {
+  userQuestion,
+  upvoteQuestion,
+  bookmarkQuestion,
+  getAllQuestionsForCollege,
+  getQuestionByIdForCollege,
+} from "../controllers/questionController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const questionRoutes = Router();
 
 questionRoutes.route("/ask-question").post(verifyToken, userQuestion);
+
+questionRoutes.route("/upvote-question").post(verifyToken, upvoteQuestion);
+
+questionRoutes.route("/bookmark-question").post(verifyToken, bookmarkQuestion);
+
+questionRoutes
+  .route("/get-all-question-college")
+  .post(verifyToken, getAllQuestionsForCollege);
+
+questionRoutes
+  .route("/get-question-byId")
+  .post(verifyToken, getQuestionByIdForCollege);
