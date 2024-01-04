073635609335f6b4e1de316bf7dee4c77b537dec
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-04 21:58:18 +0530

    Refactor userSignup, userLogin, getQuestion, upvoteQuestion, downVoteQuestion, and getResponses functions

--- client/app/lib/actions.js
+++ client/app/lib/actions.js
@@ -1,60 +1,74 @@
 // action for login of the user
-import axios from 'axios';
+import axios from "axios";
 
-export async function userSignup (info) {
-    try {
-        const response = await axios.post('http://localhost:8080/api/v1/auth/register', info)
-        console.log(response.data);
-        
-
-    } catch (error) {
-        return false
+export async function userSignup(info) {
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/api/v1/auth/register",
+      info
+    );
+    // console.log(response.data);
+    if (response) {
+      return true;
     }
-    
-};
-
-export  async function userLogin () {
-    //login function to be added
-    // if success returns true 
-    return true
+    return false;
+  } catch (error) {
+    return false;
+  }
 }
 
-export async function getQuestion (collegeId) {
-    try {
-        // correct the api
-        // check that the return format is correct
+export async function userLogin(info) {
+  //login function to be added
+  // if success returns true
+  console.log(info);
+  const response = await axios.post(
+    "http://localhost:8080/api/v1/auth/login",
+    info
+  );
+  console.log(response);
+  if (response) {
+    return true;
+  }
+}
 
-        const response = await axios.get('http://localhost:8080/api/v1/get-all-question-college', collegeId );
-        
-        return {questions: response.data.questions}
-    } catch (error) {
-        throw new Error(error);
-    }
+export async function getQuestion(collegeId) {
+  try {
+    // correct the api
+    // check that the return format is correct
+
+    const response = await axios.get(".../getAllQuestionForCollege", collegeId);
+
+    return { questions: response.data.questions };
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export async function upvoteQuestion(questionId) {
-    try {
-        const response = await axios.post('http://localhost:8080/api/v1/upvote-question', questionId);
+  try {
+    const response = await axios.post("....api to upvote", questionId);
 
-        return(response.data.updatedQuestion);
-    } catch (error) {
-        throw new Error(error);
-    }
+    return response.data.updatedQuestion;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-export async function downVoteQuestion (questionId) {
-    try {
-        const response = await axios.post('http://localhost:8080/api/v1/downvote-question', questionId)
-        return (response.data.updatedQuestion);
-    } catch (error) {
-        throw new Error (error);
-    }
+export async function downVoteQuestion(questionId) {
+  try {
+    const response = await axios.post("...api for downvote", questionId);
+    return response.data.updatedQuestion;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-export async function getResponses (quesitonId) => { 
-    try {
-        const response = await axios
-    }
+export async function getResponses(quesitonId) {
+  try {
+    const response = await axios.get("...route to get answers", quesitonId);
+
+    return response.data.answers;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
-
-
