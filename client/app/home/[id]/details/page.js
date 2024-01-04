0319154d8bf4c4431cf314c89844d10aa3d11d79
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-04 15:07:06 +0530

    new route introduced

---
+++ client/app/home/[id]/details/page.js
@@ -0,0 +1,8 @@
+
+export default function ResponsesPage() {
+    return (
+        <div>
+            responses
+        </div>
+    )
+}
--- client/app/home/page.js
+++ client/app/home/page.js
@@ -3,7 +3,8 @@ import QuestionInterface from "../ui/home/questionInterface"
 export default function Home() {
     return(
         <div className="flex flex-col">
-            <QuestionInterface 
+            <QuestionInterface
+                id={2} 
                 title="This is question title"
                 description="this is the description of the question that means we describe it here "
                 upvotes={4}
