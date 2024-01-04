a409eb431f13125a2a24054287d75a67445b557e
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-04 16:43:09 +0530

    Add fields to college schema

--- server/src/controllers/collegeFromController.js
+++ server/src/controllers/collegeFromController.js
@@ -3,6 +3,12 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 
 export const collegReq = asyncHandler(async (req, res) => {
+  // name: { type: String, required: true },
+  // category: { type: String, required: true },
+  // students: { type: Number },
+  // rank: { type: Number },
+  // courses: [{ type: String }],
+  // reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
   try {
     const college = new College(req.body);
     await college.save();
