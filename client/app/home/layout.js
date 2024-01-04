3725ea08cb83bf2d4b5db8dbf8b29829b5b910fc
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-04 14:30:52 +0530

    styling changes to sidenav and layout

--- client/app/home/layout.js
+++ client/app/home/layout.js
@@ -5,12 +5,12 @@ import SearchBar from "../ui/searchBar";
 export default function Layout ({ children }) {
     return (
         <div className="flex justify-center overflow-hidden h-screen gap-8">
-            <div className="p-4 my-4">
+            <div className="p-4 my-4 ">
                 <Sidenav/>
             </div>
 
             <main className="pt-0 border-l border-r min-w-[600px] max-w-[600px] relative">
-                <div className="bg-transparent backdrop-blue-sm">
+                <div className="bg-slate-50">
                     <TopBar/>
                 </div>
 
--- client/app/ui/sidenav.js
+++ client/app/ui/sidenav.js
@@ -1,51 +1,51 @@
 
 export default function Sidenav() {
     return (
-        <div className="text-lg flex flex-col gap-6">
+        <div className="text-lg flex flex-col gap-2">
             <div className="">
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg"> 
                     <img src="/icons/home.svg" alt='home'/>
                     <h2>Home</h2>
                 </span>
             </div>
 
             <div>
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg">
                     <img src="/icons/search.svg" alt='explore'/>
                     <h2>Explore</h2>
                 </span>
             </div>
 
             <div>
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg">
                     <img src="/icons/group.svg" alt="community"/>
                     <h2>Community</h2>
                 </span>
             </div>
 
             <div>
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg">
                     <img src="/icons/notification.svg" alt="bell"/>
                     <h2>Notifications</h2>
                 </span>
             </div>
 
             <div>
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg">
                     <img src='/icons/profile.svg' alt="user" />
                     <h2>Profile</h2>
                 </span>
             </div>
 
             <div>
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg">
                     <img src="/icons/askQuestion.svg" alt="ask question"/>
                     <h2>Ask question</h2>
                 </span>
             </div>
 
             <div>
-                <span className="flex gap-1">
+                <span className="flex gap-1 hover:bg-gray-200 p-2 cursor-pointer rounded-lg">
                     <img src="/icons/settings.svg" alt="settings icon"/>
                     <h2>Settings</h2>
                 </span>
