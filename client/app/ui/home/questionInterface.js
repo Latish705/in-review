0ed9db021dba27b77be56206d376a5bba6687bf2
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-05 00:18:54 +0530

    question interface onclick functions added

--- client/app/ui/home/questionInterface.js
+++ client/app/ui/home/questionInterface.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
+import { questionUpvote, questionDownvote } from '@/app/lib/actions'
 
 export default function QuestionInterface({title, id, description, upvotes, downvotes, name, profilePicture}) {
+
+    const handleUpvote = async (id) => {
+        const responseOfUpvote = await questionUpvote (id)
+        upvotes++;
+    }
+    const handleDownvote = async (id) => {
+        const responseOfDownvote = await questionDownvote(id);
+        downvotes++;
+    }
     
     return (
         <section className="border-b cursor-pointer hover:bg-gray-50 pt-10">
@@ -27,15 +37,21 @@ export default function QuestionInterface({title, id, description, upvotes, down
             <div className="border-t mx-4 flex gap-4 py-2"> 
                 <div className="flex gap-5 items-center"> {/* (upvote downvote) (response) */}
                     <div className="flex gap-2">
-                        <span className="flex gap-1">
+                        <button 
+                            className="flex gap-1"
+                            onClick={()=>{handleUpvote(id)}}
+                        >
                             <img src="/icons/upvote.svg" alt="up"/>
                             <p>{upvotes}</p>
-                        </span>
+                        </button>
 
-                        <span className="flex gap-1">
+                        <button 
+                            className="flex gap-1"
+                            onClick={()=>{handleDownvote(id)}}
+                        >
                             <img src="/icons/downvote.svg" alt="down"/>
                             <p>{downvotes}</p>
-                        </span>
+                        </button>
                     </div>
 
                     <div>
