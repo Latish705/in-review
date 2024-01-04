373d4b39f9c525fe1a09a3a28a66134183f45a10
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-04 23:43:16 +0530

    Add upvote and downvote functionality to answer routes

--- server/src/routes/answer.routes.js
+++ server/src/routes/answer.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { getAllAnswers, userAnswer } from "../controllers/answerController";
+import {
+  getAllAnswers,
+  userAnswer,
+  upvoteAnswer,
+  downvoteAnswer,
+} from "../controllers/answerController";
 import {} from "../middlewares/authMiddleware";
 
 const answerRoutes = Router();
@@ -8,6 +13,7 @@ answerRoutes.route("/getAnswersFor").get(getAllAnswers);
 
 answerRoutes.route("/submitAnswer").post(userAnswer);
 
-answerRoutes.route("/upvoteAnswer").post();
+answerRoutes.route("/upvoteAnswer").post(upvoteAnswer);
+answerRoutes.route("/downvoteAnswer").post(downvoteAnswer);
 
 export default answerRoutes;
